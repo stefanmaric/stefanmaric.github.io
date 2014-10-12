19bb5c4608ec55f81a3243540d6ca0dc9f379eba
Stefan Maric <alexstefan92@gmail.com>
2014-10-12 17:09:02 -0430

    Autoprefizer's defaults are ok. Let's keep it clean.

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('styles', function() {
       // Follow includes of regular CSS
       'include css': true
     }))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(autoprefixer())
     .pipe(rename('build.css'))
     .pipe(gulp.dest('css/'));
 });
